b84331b2c7c035f516951b9933601b5d7bb45c71
Renan <renanvolkers@hotmail.com>
2024-06-12 18:13:06 -0300

    Feat(CWO-006-Interface-Repositorio):    methods that a class must implement, objective to centralize data access logic and provide an abstraction for the data layer.

---
+++ TempoCompany/Tempo.Knight.Domain/Repositories/IAttributeRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Tempo.Common.Setup.Respository;
+
+namespace Tempo.Knight.Domain.Repositories
+{
+    /// <summary>
+    /// methods that a class must implement
+    /// objective to centralize data access logic and provide an abstraction for the data layer
+    /// </summary>
+    public interface IAttributeRepository : IBaseRepository<Model.Attribute, Guid>
+    {
+
+    }
+}
