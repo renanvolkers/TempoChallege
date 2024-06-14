86576ff944f5d3362717b81876d83def7290a13d
Renan <renanvolkers@hotmail.com>
2024-06-14 16:20:57 -0300

    Feat(CWO-00015-Service):Serivces disponible for Controllers, GET, ADD Knight.

---
+++ TempoCompany/Tempo.Knight.Domain/Services/Knights/IKnightService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System.Linq.Expressions;
+using Tempo.Common.Setup.Api;
+using Tempo.Common.Setup.Service;
+using Tempo.Knight.Dto.Requests.Knight;
+using Tempo.Knight.Dto.Responses;
+
+namespace Tempo.Knight.Application.Domain.Knights
+{
+    public interface IKnightService : ITempoBaseService<Knight.Domain.Model.Knight,ResponseKnight>
+    {
+        Task<BaseResponse<List<ResponseKnight>>> GetFilterAsync(string Filter = "");
+        Task<BaseResponse<ResponseKnight>> AddKnightAsync(BaseRequest<RequestKnight> request, params Expression<Func<IModel, object>>[] references);
+
+    }
+}
