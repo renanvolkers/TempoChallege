16a8cf57b88c18432e9369198a703acd46294525
Renan <renanvolkers@hotmail.com>
2024-06-16 06:09:40 -0300

    Feat(CWO-0019-Specification): specification pattern outlines a business rule that is combinable with other business rules.

---
+++ TempoCompany/Tempo.Common.Setup/Specification/IdentitySpecification.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Tempo.Common.Setup.Specification
+{
+
+    /// <summary>
+    /// Abstract Generic for EXPRESSION in function Lambda.
+    /// specification pattern outlines a business rule that is combinable with other business rules.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal sealed class IdentitySpecification<T> : Specification<T>
+    {
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            return x => true;
+        }
+    }
+}
---
+++ TempoCompany/Tempo.Common.Setup/Specification/Specification.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace Tempo.Common.Setup.Specification
+{
+    /// <summary>
+    /// Abstract Generic for EXPRESSION in function Lambda.
+    /// specification pattern outlines a business rule that is combinable with other business rules.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Specification<T>
+        {
+            private static readonly Specification<T> All = new IdentitySpecification<T>();
+
+            public bool IsSatisfiedBy(T entity)
+            {
+                var predicate = ToExpression().Compile();
+                return predicate(entity);
+            }
+
+            public abstract Expression<Func<T, bool>> ToExpression();
+
+
+        }
+    
+}
