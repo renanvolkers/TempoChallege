8421397928ea70928fb893cea61df367349d9571
Renan <renanvolkers@hotmail.com>
2024-06-17 11:02:24 -0300

    Feat(CWO-0028-Context): Register the DbContext with the connection string from DataBaseConfig

---
+++ TempoCompany/Tempo.Knight.Infra/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tempo.Knight.Infra
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static IServiceCollection AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register the DataBaseConfig as a configuration instance
+            services.Configure<DataBaseConfig>(configuration.GetSection("DataBaseConfig"));
+
+            // Register the DbContext with the connection string from DataBaseConfig
+            services.AddDbContext<KnightDbContext>((serviceProvider, options) =>
+            {
+                var databaseConfig = serviceProvider.GetRequiredService<IOptions<DataBaseConfig>>().Value;
+                options.UseSqlServer(databaseConfig.ConnectionString);
+            });
+
+            // Optionally, register other services here
+
+            return services;
+        }
+
+
+
+    }
+}
---
+++ TempoCompany/Tempo.Knight.Infra/KnightDbContext .cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Tempo.Knight.Infra
+{
+    /// <summary>
+    /// model configurations, and ensure that the service registration and connection strings are correctly configuredg
+    /// </summary>
+    public class KnightDbContext : DbContext
+    {
+        public KnightDbContext(DbContextOptions<KnightDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Model.Knight> Knights { get; set; }
+        public DbSet<Domain.Model.Attribute> Attributes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Domain.Model.Knight>().ToTable("Knights");
+            modelBuilder.Entity<Domain.Model.Attribute>().ToTable("Attributes");
+        }
+    }
+}
