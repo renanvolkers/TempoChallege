23703b2c0886287c3375c998556040bed289da18
Renan <renanvolkers@hotmail.com>
2024-06-17 10:34:55 -0300

    Feat(CWO-0026-Injection-Dependency): Configuration Injection Dependency Service, Repository.

---
+++ TempoCompany/Tempo.Knight.Api/Config/IocExtension.cs
@@ -0,0 +1,34 @@
+﻿using Tempo.Knight.Application.Domain.Knights;
+using Tempo.Knight.Application.Manager.Calculator;
+using Tempo.Knight.Application.Services.Knights;
+using Tempo.Knight.Application.Specification;
+using Tempo.Knight.Application.Validations.Knight;
+using Tempo.Knight.Domain.Model;
+using Tempo.Knight.Domain.Model.Calculator;
+using Tempo.Knight.Domain.Repositories;
+using Tempo.Knight.Dto.Requests.Knight;
+using Tempo.Knight.Dto.Responses;
+using FluentValidation;
+using Tempo.Common.Setup.Api;
+using Tempo.Knight.Infra.Repositories;
+
+namespace Tempo.Knight.Api.Config
+{
+    public static class IocExtension
+    {
+        public static void AddSevicesTempo(this IServiceCollection services)
+        {
+            services.AddScoped<IKnightsRepository, KnightsRepository>();
+            services.AddScoped<IKnightService, KnightService>();
+            services.AddScoped<IFilterStrategy<Domain.Model.Knight>, FilterKnightsByHeroesSpecification>();
+            services.AddScoped<IManagerCalculator<Domain.Model.Knight, ResponseKnight>, ManagerCalculator>();
+            services.AddScoped<IAttackCalculator, AttackCalculator>();
+            services.AddScoped<IAttributeRepository, AttributeRepository>();
+            
+            services.AddScoped<IExperienceCalculator, ExperienceCalculator>();
+            services.AddTransient<IValidator<RequestKnight>, AddKnightValidator>();
+            services.AddTransient<TempoApiController>();
+        }
+    }
+
+}
