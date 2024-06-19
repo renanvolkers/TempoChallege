80ee1aad39202282112e220c7d58a0c0e36f38db
Renan <renanvolkers@hotmail.com>
2024-06-18 23:35:09 -0300

    Feat(CWO-0034-Repository): Repository acess data.

---
+++ TempoCompany/Tempo.Knight.Infra/Repositories/WeaponRepository.cs
@@ -0,0 +1,11 @@
+﻿using Tempo.Common.Setup.Service;
+using Tempo.Knight.Domain.Repositories;
+
+namespace Tempo.Knight.Infra.Repositories
+{
+    public class WeaponRepository : BaseRepository<KnightDbContext, Domain.Model.Weapon, Guid>, IWeaponRepository
+    {
+        public WeaponRepository(KnightDbContext context) : base(context) { }
+
+    }
+}
