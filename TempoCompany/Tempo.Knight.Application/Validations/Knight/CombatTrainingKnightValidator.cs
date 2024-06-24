e0a4890255b71028151630d5b1ec5a10cbe5c2cd
Renan <renanvolkers@hotmail.com>
2024-06-23 23:39:32 -0300

    Feat(CWO-0055-Validation): Validation Request Training  values.

---
+++ TempoCompany/Tempo.Knight.Application/Validations/Knight/CombatTrainingKnightValidator.cs
@@ -0,0 +1,25 @@
+﻿using Tempo.Knight.Dto.Requests.Knight;
+using FluentValidation;
+
+namespace Tempo.Knight.Application.Validations.Knight
+{
+    public class CombatTrainingKnightValidator : AbstractValidator<RequestTrainingKnight>
+    {
+        /// <summary>
+        /// Rules basic for validation Knight
+        /// </summary>
+        public CombatTrainingKnightValidator()
+        {
+            RuleFor(x => x.CombatTraining)
+                .LessThan(0).WithMessage("Point must be more than 0")
+                .NotNull()
+                .NotEmpty();
+            RuleFor(x => x.CombatTraining)
+                .GreaterThan(10).WithMessage("Point must be less than 10")
+                .NotNull()
+                .NotEmpty();
+
+        }
+
+    }
+}
