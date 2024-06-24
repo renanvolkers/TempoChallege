3746bf42bbe21f7f85fa183d2a2628c89a7038a7
Renan <renanvolkers@hotmail.com>
2024-06-23 23:29:44 -0300

    Feat(CWO-0054-DTO): Request Training DTO

---
+++ TempoCompany/Tempo.Knight.Dto/Requests/Knight/RequestTrainingKnight.cs
@@ -0,0 +1,14 @@
+﻿using Tempo.Common.Setup;
+
+namespace Tempo.Knight.Dto.Requests.Knight
+{
+
+    /// <summary>
+    /// Request for training controller
+    /// </summary>
+    public class RequestTrainingKnight : IRequest
+    {
+        public float CombatTraining { get; set; }
+
+    }
+}
