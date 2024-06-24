009d3b0e23f027d5d8c57a59a050ba6fbd6d4321
Renan <renanvolkers@hotmail.com>
2024-06-24 18:28:50 -0300

    Feat(CWO-0056-Sql):Script start database.

--- 01-BaseTempo.sql
+++ 01-BaseTempo.sql
@@ -24,7 +24,13 @@ CREATE TABLE dbo.Knights (
     Birthday DATE NOT NULL,
     KeyAttribute NVARCHAR(255) NOT NULL,
     CharacterType NVARCHAR(255) NOT NULL,
-     HallOfHeroes BIT DEFAULT 0 NOT NULL
+    HallOfHeroes BIT DEFAULT 0 NOT NULL,
+	CombatTraining float DEFAULT 0.0 NOT NULL,
+	CreatedBy NVARCHAR(255) NOT NULL,
+	CreatedAt DATETIME NOT NULL,
+    ModifiedBy NVARCHAR(255) NOT NULL,
+    ModifiedAt DATETIME 
+	 
 );
 
 -- Tabela Weapon
@@ -35,6 +41,10 @@ CREATE TABLE dbo.Weapons (
     Attr NVARCHAR(255) NOT NULL,
     Equipped BIT NOT NULL,
     KnightId UNIQUEIDENTIFIER,
+	CreatedBy NVARCHAR(255) NOT NULL,
+	CreatedAt DATETIME NOT NULL,
+    ModifiedBy NVARCHAR(255) NOT NULL,
+    ModifiedAt DATETIME, 
     FOREIGN KEY (KnightId) REFERENCES Knights(Id)
 );
 
@@ -43,13 +53,22 @@ CREATE TABLE dbo.Attributes (
     Id UNIQUEIDENTIFIER PRIMARY KEY,
     Name NVARCHAR(255) NOT NULL,   
 	Value INT NOT NULL,
+	CreatedBy NVARCHAR(255) NOT NULL,
+	CreatedAt DATETIME NOT NULL,
+    ModifiedBy NVARCHAR(255) NOT NULL,
+    ModifiedAt DATETIME 
 
 );
 
 -- Tabela KnightAttribute
 CREATE TABLE dbo.KnightAttributes (
-    KnightId UNIQUEIDENTIFIER NOT NULL,
+    Id UNIQUEIDENTIFIER ,
+	KnightId UNIQUEIDENTIFIER NOT NULL,
     AttributeId UNIQUEIDENTIFIER NOT NULL,
+	CreatedBy NVARCHAR(255) NOT NULL,
+	CreatedAt DATETIME NOT NULL,
+    ModifiedBy NVARCHAR(255) NOT NULL,
+    ModifiedAt DATETIME, 
     PRIMARY KEY (KnightId, AttributeId),
     FOREIGN KEY (KnightId) REFERENCES Knights(Id),
     FOREIGN KEY (AttributeId) REFERENCES Attributes(Id)
--- 02-BaseTempo.sql
+++ 02-BaseTempo.sql
@@ -1,71 +1,72 @@
 USE TempoChallengeKnights
 GO
 
- -- Inserir um Knight
-INSERT INTO Knights (Id, Name, Nickname, Birthday, KeyAttribute, CharacterType, HallOfHeroes)
+-- Inserir um Knight com informações de auditoria
+INSERT INTO Knights (Id, Name, Nickname, Birthday, KeyAttribute, CharacterType, HallOfHeroes, CreatedBy, CreatedAt, ModifiedBy, ModifiedAt)
 VALUES
-    (NEWID(), 'Arthur', 'The Brave', '1980-01-01', 'Strength', 'Warrior', 0),
-    (NEWID(), 'Lancelot', 'The Gallant', '1975-05-15', 'Courage', 'Knight', 1),
-    (NEWID(), 'Galahad', 'The Pure', '1990-03-20', 'Virtue', 'Paladin', 1),
-    (NEWID(), 'Merlin', 'The Wise', '1950-11-11', 'Wisdom', 'Mage', 1),
-    (NEWID(), 'Guinevere', 'The Fair', '1978-09-30', 'Charm', 'Royalty', 0),
-    (NEWID(), 'Tristan', 'The Loyal', '1985-04-25', 'Loyalty', 'Knight', 0),
-    (NEWID(), 'Isolde', 'The Beloved', '1982-07-15', 'Love', 'Lady', 0),
-    (NEWID(), 'Gawain', 'The Valiant', '1970-02-18', 'Valor', 'Knight', 1),
-    (NEWID(), 'Mordred', 'The Betrayer', '1988-06-05', 'Deception', 'Rogue', 0),
-    (NEWID(), 'Percival', 'The Seeker', '1995-08-22', 'Quest', 'Paladin', 0),
-    (NEWID(), 'Morgana', 'The Enchantress', '1965-12-03', 'Magic', 'Sorceress', 0),
-    (NEWID(), 'Bors', 'The Faithful', '1973-03-08', 'Faith', 'Knight', 1),
-    (NEWID(), 'Elaine', 'The Graceful', '1987-01-14', 'Grace', 'Lady', 0),
-    (NEWID(), 'Nimue', 'The Mysterious', '1992-10-20', 'Mystery', 'Sorceress', 0),
-    (NEWID(), 'Perceval', 'The Righteous', '1977-06-28', 'Justice', 'Paladin', 1);
+    (NEWID(), 'Arthur', 'The Brave', '1980-01-01', 'Strength', 'Warrior', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Lancelot', 'The Gallant', '1975-05-15', 'Courage', 'Knight', 1, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Galahad', 'The Pure', '1990-03-20', 'Virtue', 'Paladin', 1, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Merlin', 'The Wise', '1950-11-11', 'Wisdom', 'Mage', 1, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Guinevere', 'The Fair', '1978-09-30', 'Charm', 'Royalty', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Tristan', 'The Loyal', '1985-04-25', 'Loyalty', 'Knight', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Isolde', 'The Beloved', '1982-07-15', 'Love', 'Lady', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Gawain', 'The Valiant', '1970-02-18', 'Valor', 'Knight', 1, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Mordred', 'The Betrayer', '1988-06-05', 'Deception', 'Rogue', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Percival', 'The Seeker', '1995-08-22', 'Quest', 'Paladin', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Morgana', 'The Enchantress', '1965-12-03', 'Magic', 'Sorceress', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Bors', 'The Faithful', '1973-03-08', 'Faith', 'Knight', 1, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Elaine', 'The Graceful', '1987-01-14', 'Grace', 'Lady', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Nimue', 'The Mysterious', '1992-10-20', 'Mystery', 'Sorceress', 0, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Perceval', 'The Righteous', '1977-06-28', 'Justice', 'Paladin', 1, 'system', GETDATE(), 'system', GETDATE());
 
- -- Inserir um Attribute
-INSERT INTO Attributes (Id, Name, Value)
-VALUES
-    (NEWID(), 'Strength', 100),
-    (NEWID(), 'Courage', 90),
-    (NEWID(), 'Virtue', 95),
-    (NEWID(), 'Wisdom', 85),
-    (NEWID(), 'Charm', 80),
-    (NEWID(), 'Loyalty', 88),
-    (NEWID(), 'Love', 92),
-    (NEWID(), 'Valor', 87),
-    (NEWID(), 'Deception', 75),
-    (NEWID(), 'Quest', 82),
-    (NEWID(), 'Magic', 89),
-    (NEWID(), 'Faith', 91),
-    (NEWID(), 'Grace', 78),
-    (NEWID(), 'Mystery', 86),
-    (NEWID(), 'Justice', 84);
 
- -- Inserir uma Weapon
-INSERT INTO Weapons (Id, Name, Mod, Attr, Equipped, KnightId)
+-- Inserir um Attribute com informações de auditoria
+INSERT INTO Attributes (Id, Name, Value, CreatedBy, CreatedAt, ModifiedBy, ModifiedAt)
 VALUES
-    (NEWID(), 'Excalibur', 10, 'Strength', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Arthur')),
-    (NEWID(), 'Holy Lance', 8, 'Valor', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Gawain')),
-    (NEWID(), 'Enchanted Bow', 7, 'Magic', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Tristan')),
-    (NEWID(), 'Shield of Faith', 5, 'Faith', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Bors')),
-    (NEWID(), 'Sword of Justice', 9, 'Justice', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Perceval'));
+    (NEWID(), 'Strength', 100, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Courage', 90, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Virtue', 95, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Wisdom', 85, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Charm', 80, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Loyalty', 88, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Love', 92, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Valor', 87, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Deception', 75, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Quest', 82, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Magic', 89, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Faith', 91, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Grace', 78, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Mystery', 86, 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Justice', 84, 'system', GETDATE(), 'system', GETDATE());
 
- -- Inserir um KnightAttribute
-INSERT INTO KnightAttributes (KnightId, AttributeId)
+-- Inserir uma Weapon com informações de auditoria
+INSERT INTO Weapons (Id, Name, Mod, Attr, Equipped, KnightId, CreatedBy, CreatedAt, ModifiedBy, ModifiedAt)
 VALUES
-    ((SELECT TOP 1 Id FROM Knights WHERE Name = 'Arthur'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Strength')),
-    ((SELECT TOP 1 Id FROM Knights WHERE Name = 'Lancelot'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Courage')),
-    ((SELECT TOP 1 Id FROM Knights WHERE Name = 'Galahad'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Virtue')),
-    ((SELECT TOP 1 Id FROM Knights WHERE Name = 'Merlin'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Wisdom')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Guinevere'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Charm')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Tristan'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Loyalty')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Isolde'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Love')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Gawain'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Valor')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Mordred'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Deception')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Percival'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Quest')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Morgana'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Magic')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Bors'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Faith')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Elaine'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Grace')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Nimue'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Mystery')),
-    ((SELECT TOP 1  Id FROM Knights WHERE Name = 'Perceval'), (SELECT TOP 1 Id FROM Attributes WHERE Name = 'Justice'));
+    (NEWID(), 'Excalibur', 10, 'Strength', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Arthur'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Holy Lance', 8, 'Valor', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Gawain'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Enchanted Bow', 7, 'Magic', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Tristan'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Shield of Faith', 5, 'Faith', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Bors'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), 'Sword of Justice', 9, 'Justice', 1, (SELECT TOP 1 Id FROM Knights WHERE Name = 'Perceval'), 'system', GETDATE(), 'system', GETDATE());
+
+-- Inserir um KnightAttribute com informações de auditoria
+INSERT INTO KnightAttributes (Id, KnightId, AttributeId, CreatedBy, CreatedAt, ModifiedBy, ModifiedAt)
+VALUES
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Arthur'), (SELECT Id FROM Attributes WHERE Name = 'Strength'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Lancelot'), (SELECT Id FROM Attributes WHERE Name = 'Courage'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Galahad'), (SELECT Id FROM Attributes WHERE Name = 'Virtue'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Merlin'), (SELECT Id FROM Attributes WHERE Name = 'Wisdom'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Guinevere'), (SELECT Id FROM Attributes WHERE Name = 'Charm'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Tristan'), (SELECT Id FROM Attributes WHERE Name = 'Loyalty'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Isolde'), (SELECT Id FROM Attributes WHERE Name = 'Love'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Gawain'), (SELECT Id FROM Attributes WHERE Name = 'Valor'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Mordred'), (SELECT Id FROM Attributes WHERE Name = 'Deception'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Percival'), (SELECT Id FROM Attributes WHERE Name = 'Quest'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Morgana'), (SELECT Id FROM Attributes WHERE Name = 'Magic'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Bors'), (SELECT Id FROM Attributes WHERE Name = 'Faith'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Elaine'), (SELECT Id FROM Attributes WHERE Name = 'Grace'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Nimue'), (SELECT Id FROM Attributes WHERE Name = 'Mystery'), 'system', GETDATE(), 'system', GETDATE()),
+    (NEWID(), (SELECT Id FROM Knights WHERE Name = 'Perceval'), (SELECT Id FROM Attributes WHERE Name = 'Justice'), 'system', GETDATE(), 'system', GETDATE());
 
 
 GO
