6c3ed3a6b7cc672490167911145d104846ee0439
Renan <renanvolkers@hotmail.com>
2024-06-26 16:45:06 -0300

    Feat(CWO-0058-Paginate-List): List Pagination Kningh

---
+++ Tempo.Knight.Front/Tempo.Knight.WebApp/Components/Pages/KnightForm/KnightViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Tempo.Knight.WebApp.Components.Pages.KnightForm
+{
+    public class KnightViewModel
+    {
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public List<WeaponViewModel> Weapons { get; set; } = new();
+        public required string KeyAttribute { get; set; }
+        public int Attack { get; set; }
+        public int Experience { get; set; }
+        public Guid Id { get; set; }
+    }
+}
---
+++ Tempo.Knight.Front/Tempo.Knight.WebApp/Components/Pages/KnightForm/Pagination.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Tempo.Knight.WebApp.Components.Pages.KnightForm
+{
+    public partial class Pagination
+    {
+        [Parameter] public int TotalItems { get; set; }
+        [Parameter] public int ItemsPerPage { get; set; } = 10;
+        [Parameter] public EventCallback<int> OnPageChanged { get; set; }
+
+        private int CurrentPage { get; set; } = 1;
+
+        private int TotalPages => (int)Math.Ceiling((double)TotalItems / ItemsPerPage);
+
+        private   void SelectPageAsync(int page)
+        {
+            if (page >= 1 && page <= TotalPages)
+            {
+                CurrentPage = page;
+                 OnPageChanged.InvokeAsync(CurrentPage);
+                StateHasChanged();
+
+            }
+        }
+    }
+}
