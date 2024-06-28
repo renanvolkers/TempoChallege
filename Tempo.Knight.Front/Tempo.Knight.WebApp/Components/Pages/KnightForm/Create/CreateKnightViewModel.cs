c8eac35bc15e5cf3b998462dbc1b8a65d90d0249
Renan <renanvolkers@hotmail.com>
2024-06-27 23:59:33 -0300

    Feat(CWO-0068-VEIWMODWL):NEW VIEW MODEL

---
+++ Tempo.Knight.Front/Tempo.Knight.WebApp/Components/Pages/KnightForm/Create/CreateKnightViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Tempo.Knight.WebApp.Components.Pages.KnightForm
+{
+    public class CreateKnightViewModel
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public List<WeaponViewModel> Weapons { get; set; } = new();
+        public string KeyAttribute { get; set; } = "";
+        public int Attack { get; set; }
+        public int Experience { get; set; }
+        public Guid Id { get; set; }
+        public CreateKnightViewModel()
+        {
+
+        }
+    }
+
+}
